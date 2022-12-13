3de0ae681a166d1cdde494d695c4b05860678989
vall.turu.alex <vall.turu.alex@campus.copernic.cat>
2022-12-13 12:16:22 +0100

    Merge from DevAlex

--- src/main/java/presentation/PrimaryController.java
+++ src/main/java/presentation/PrimaryController.java
@@ -255,7 +255,6 @@ public class PrimaryController implements Initializable {
     //CUSTOMER 
 
     @FXML
-    private Button bt_aniadir, bt_actualizar, bt_eliminar, bt_limpiar;
     @FXML
     private TableView tv_customer;
     @FXML
@@ -365,6 +364,7 @@ public class PrimaryController implements Initializable {
         return customer;
     }
 
+    /**
     private void desactivaSeleccio() {
         //deshabilitem botóns i fila seleccionada
         bt_actualizar.setDisable(true);
